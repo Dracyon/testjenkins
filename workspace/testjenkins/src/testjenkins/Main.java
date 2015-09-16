327d742d0a6f0ead5942ccafaf63e748446b4094
Wojciech Kowalczyk <wojciech.kowalczyk@open-rnd.pl>
2015-09-16 12:20:06 +0200

    little

--- workspace/testjenkins/src/testjenkins/Main.java
+++ workspace/testjenkins/src/testjenkins/Main.java
@@ -1,14 +1,40 @@
 package testjenkins;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("siemano");
-		int j = 10;
-		j=j+30;
-		System.out.println("Srsly?");
-		System.out.println("mao");
+		System.out.println("hello jenkins");
+		File src = new File ("/Users/wkowalczyk/Documents/workspace/MantaTesting/Files/Orginal/Binsy/");
+		File dest = new File ("/Users/wkowalczyk/Documents/workspace/testjenkins/Binsy");
+		try {
+			FileUtils.copyDirectory(src, dest);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		PrintWriter out = null;
+//		try {
+//			out = new PrintWriter("BinsyErrorPathListBL.txt");
+//		} catch (FileNotFoundException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//		try {
+//			FileInputStream fis = new FileInputStream("test.txt");
+//			BufferedReader fisReader = new BufferedReader(new InputStreamReader(fis));
+//			
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+		
+		
 	}
+		
 
 }
